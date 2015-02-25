17225881c96d6314777ff6527e76cce7c05a51c2
jaiprakash sharma <sharmajaip@yahoo.com>
2015-02-25 13:27:49 +0000

    app global

---
+++ ConsoleApplication3/App-GlobalResources/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3.App_GlobalResources
+{
+    class Class1
+    {
+    }
+}
