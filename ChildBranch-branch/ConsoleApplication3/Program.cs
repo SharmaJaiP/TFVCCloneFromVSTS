171bfb048b88abef1fb7463f8a2161035836f802
jaiprakash sharma <sharmajaip@yahoo.com>
2016-11-24 12:10:13 +0000

    Branched from $/TFVC/ChildBranch

---
+++ ChildBranch-branch/ConsoleApplication3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //One checkin
+            //One checkin
+            // anothe chekcin
+            // anpother
+             // another checkin
+             // Adding new text file.
+             // doing another checkin.
+        }
+
+        static void PrintStatus()
+        {
+            // Adding another method
+            // Making change in child branch
+        }
+    }
+}
+
