e4518609fcd267a2bc958bb0a05d9918fc7bf19c
jaiprakash sharma <sharmajaip@yahoo.com>
2017-11-02 10:11:17 +0000

    Branched from $/TFVC/ConsoleApplication3

---
+++ ConsoleApplication3-ForDemo/ConsoleApplication3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+
+        static void Foo()
+        {
+        }
+
+        static void Bar()
+        {
+        }
+        static void Main(string[] args)
+        {
+             // 
+            //One checkin
+            // anothe chekcin
+            // anpother
+             // another checkin
+             // Adding new text file.
+             // doing another checkin.
+             // another checkin
+             // adding a binary file.
+        }
+
+        static void PrintStatus()
+        {
+            // Adding another method
+            // testing Sprint 82 binary upgrade
+            // second checkin
+        }
+    }
+}
